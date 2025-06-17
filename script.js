ac4e05d817d820430a1d11d47a41676b741ab3f8
Yasircondingbt <185803128+Yasircodingbt@users.noreply.github.com>
2025-06-17 21:33:34 +0500

    javascript logic building

---
+++ script.js
@@ -0,0 +1,52 @@
+// 3) Create a program that counts vowels in a given string.
+//  let str = "javascript";
+//  var count = 0;
+// for (let val of str) {
+//     if( val === 'a' ||
+//         val === 'e' ||
+//         val === 'i' ||
+//         val === 'o' ||
+//         val === 'u' ){
+//           count++;
+
+//         }
+// }
+// console.log(count);
+
+// 2) Write a function that reverses a given string.
+// function reverse(str) {
+//     var strRev = "";
+// for (var i = str.length - 1; i >= 0; i--) {
+//    console.log(strRev += str[i]);
+//    }
+//           return strRev;
+// }
+//   var str = "yasir";
+//   console.log("%s",reverse(str));
+
+// 3) Create an array of marks. Write a function that returns the average of marks.✔️
+// let arr_average= (marks) =>{
+//    var sum = 0;
+//   for(let i = 0 ; i < marks.length ; i++){
+//           sum += marks[i]/marks.length;
+
+//   }
+//         return sum;
+// }
+
+//  let marks = [1,2,3,4,5,6,78,88,9];
+//     console.log(arr_average(marks));
+
+/** 4) Make a loop that prints only numbers divisible by 5 from 1 to 50.
+
+  for(var i = 1; i <= 50 ; i++){
+      if(i%5 === 0){
+         console.log(i);
+      }
+  }
+  */
+
+/*
+   5)
+
+   */
